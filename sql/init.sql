d47a3e6b1fbc72bf54752a19df61ed2cd2d41eee
jeanSagaz <jeantf@gmail.com>
2023-08-02 13:02:27 -0300

    adicionando script para criação do banco de dados utilizando 'mssql-tools'

---
+++ sql/init.sql
@@ -0,0 +1,26 @@
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'poc')
+BEGIN
+  CREATE DATABASE poc;
+END;
+GO
+
+USE poc
+GO
+
+IF OBJECT_ID('Customers', 'U') IS NULL
+BEGIN
+
+  CREATE TABLE [dbo].[Customers](
+	[Id] [uniqueidentifier] NOT NULL,
+	[Name] [varchar](100) NOT NULL,
+	[Email] [varchar](100) NOT NULL,
+	[Created_At] [datetime2](7) NOT NULL,
+	[Updated_At] [datetime2](7) NULL,
+ CONSTRAINT [PK_Customers] PRIMARY KEY CLUSTERED 
+(
+	[Id] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+END;
+GO
